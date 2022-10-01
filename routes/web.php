c79c4ffc207d1ae72816fa279aa306cb57cfb02b
star <sergrom12@yandex.ru>
2022-10-01 08:52:44 +0400

    3. Первый controller в laravel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\MyPlaceController;
+use App\Http\Controllers\MyTestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return 'asdfsadf';
 });
+
+
+Route::get('/bible', [ MyPlaceController::class, 'index'] );
