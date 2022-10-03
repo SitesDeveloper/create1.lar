4cf80c8f725fb4a6312d9194f214ac152620caca
star <sergrom12@yandex.ru>
2022-10-03 10:07:16 +0400

    12. Модель. Комбинированные методы создания и обновления данных

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,82 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index() {
-        $post = Post::find(1);
-        dump($post->likes);
+        $posts = Post::where('is_published',0)->first();
+        //foreach ($posts as $post) dump($post->title);
+        dump($posts);
     }
+
+    public function create() {
+        $postsArr = [
+            [
+            'title' => 'Статья Васи',
+            'content' => 'Контент статьи Васи',
+            'image' => 'image_vasya.jpb',
+            'likes' => 32,
+            'is_published' => 1,
+            ],
+            [
+                'title' => 'Статья Пети',
+                'content' => 'Контент статьи Пети',
+                'image' => 'image_petya.jpb',
+                'likes' => 12,
+                'is_published' => 1,
+            ]
+        ];
+
+        foreach ($postsArr as $post)
+            Post::create($post);
+        
+        dd('create');
+    }
+
+    public function update() {
+        $post = Post::find(6);
+
+        $post->update([
+            'content' => 'update Контент статьи',
+        ]);
+        dump($post->title);
+    }
+
+    public function delete() {
+        //$post = Post::find(2);
+        //$post->delete();
+
+        $post = Post::withTrashed()->find(2);
+        $post->restore();
+        dump('restored');
+    }
+
+    public function firstOrCreate() {
+        $post = Post::firstOrCreate(
+            ['title'=>'Статья Ксюши'],
+            [
+                'title' => 'Статья Ксюши',
+                'content' => 'some conent',
+                'image' => 'some_image_vasya.jpb',
+                'likes' => 34332,
+                'is_published' => 1,
+            ]
+        );
+        dump($post->content);
+        dd('finished');
+    }
+
+
+    public function updateOrCreate() {
+        $post = Post::updateOrCreate(
+            ['title'=>'Статья Ани'],
+            [
+                'title' => 'Статья Ани',
+                'content' => 'some conent 11',
+                'image' => 'some_image_vasya.jpb',
+                'likes' => 322,
+                'is_published' => 1,
+            ]
+        );
+        dump($post->content);
+        dd('finished');
+    }
+
 }
