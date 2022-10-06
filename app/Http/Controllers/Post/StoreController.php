f195c8f9cfb9ae1cba7906da954f535a50f675ca
star <sergrom12@yandex.ru>
2022-10-06 10:49:41 +0400

    26. Класс Request в Laravel

--- app/Http/Controllers/Post/StoreController.php
+++ app/Http/Controllers/Post/StoreController.php
@@ -4,18 +4,13 @@ namespace App\Http\Controllers\Post;
 
 use App\Models\Post;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\StoreRequest;
 
 class StoreController extends Controller
 {
-    public function __invoke()
+    public function __invoke(StoreRequest $request)
     {
-        $data = request()->validate([
-            'title' => 'required|string',
-            'content' => 'required|string',
-            'image'=> 'required|string',
-            'category_id' => '',
-            'tags' => '',
-        ]);
+        $data = $request->validated();
         $tags = $data['tags'];
         unset($data['tags']);
 
