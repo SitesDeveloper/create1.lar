b81e5779e33739604c05be6fa43953d79f10c2c4
star <sergrom12@yandex.ru>
2022-10-08 13:23:03 +0400

    28. Классы Factory и Seed в Laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Tag;
+use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +18,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        //$posts = Post::factory(10)->make(); //make - не помещать в базу
+        Category::factory(20)->create();
+        $tags = Tag::factory(50)->create();
+        $posts = Post::factory(200)->create(); //create - помещать в базу
+
+        foreach ($posts as $post) {
+            $tagsId = $tags->random(5)->pluck('id');
+            $post->tags()->attach($tagsId);
+        }
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
